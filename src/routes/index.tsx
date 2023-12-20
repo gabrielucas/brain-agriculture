8696f817b65180e1bf46e4794ce04da106e9b6c1
Lucas Viana <lucasviana.dev@gmail.com>
2023-12-20 17:18:06 -0300

    reorganizing layout files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { Dashboard } from '../components/pages/Dashboard';
 import { Farm } from '../components/pages/Farm';
-import { DefaultLayout } from '../layouts';
+import { DefaultLayout } from '../components/layouts';
+import { Dashboard } from '../components/pages/Dashboard';
 
 export const Router: FC = () => {
   return (
