f3830e0697c85e4046d5dc3d247941a9eeacbdfe
Lucas Viana <lucasviana.dev@gmail.com>
2023-12-21 01:31:59 -0300

    test: configure coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,18 @@
 export default {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.test.{js,jsx,ts,tsx}'],
+  coverageReporters: ['json-summary', 'text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'js', 'tsx', 'jsx'],
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
@@ -7,4 +21,5 @@ export default {
     '\\.(giff|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
   },
   setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts'],
+  testMatch: ['src/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
 };
